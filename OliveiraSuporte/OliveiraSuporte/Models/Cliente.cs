9d5ec8d9b6ff300810557122cb993187ac65d121
Ekson Guevara Rocabado <edson_guevara@hotmail.com>
2012-06-15 09:25:33 -0400

    Correção cadastro
    Foi feita a tentativa de corrigir um pouco do cadastro, incluindo
    validação para email e colocando uma quantidade mínima e máxima para o
    telefone, também incluindo um link para página inicial..após efetuado o
    cadastro.

--- OliveiraSuporte/OliveiraSuporte/Models/Cliente.cs
+++ OliveiraSuporte/OliveiraSuporte/Models/Cliente.cs
@@ -11,16 +11,18 @@ namespace OliveiraSuporte.Models
         public int ClienteId { get; set; }
 
         [Required(ErrorMessage = "Este campo deve ser preenchido")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "O campo deve conter entre 3 e 50 caracteres")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "O campo deve conter entre 3 e 50 caracteres")]
         public string Nome { get; set; }
         
         [Display(Name = "Nome Da Empresa")]
         public string NomeEmpresa { get; set; }
 
         [DataType(DataType.EmailAddress)]
+        [RegularExpression(("^[a-z0-9_\\+-]+(\\.[a-z0-9\\+-]+)*@[a-z0-9]+(\\.[a-z0-9-]+)*\\.([a-z]{2,4})$"), ErrorMessage = "Email Inválido")]
         public string Email { get; set; }
         
         [Required(ErrorMessage = "O preenchimento deste campo é obrigatorio")]
+        [StringLength(12,MinimumLength = 8, ErrorMessage = "O número deve conter no mínimo 8 digitos")]
         [RegularExpression(@"^([0-9]+)?$",ErrorMessage = "O campo deve possuir somente números")]
         public string Telefone { get; set; }
         
